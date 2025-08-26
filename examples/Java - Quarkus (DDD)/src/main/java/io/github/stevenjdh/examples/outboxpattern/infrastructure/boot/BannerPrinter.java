6e55a37cc27c13a0a5627b167b0521876c1c82d9
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2025-08-26 20:52:00 +0200

    Updated custom banner logic to be configurable

--- examples/Java - Quarkus (DDD)/src/main/java/io/github/stevenjdh/examples/outboxpattern/infrastructure/boot/BannerPrinter.java
+++ examples/Java - Quarkus (DDD)/src/main/java/io/github/stevenjdh/examples/outboxpattern/infrastructure/boot/BannerPrinter.java
@@ -14,25 +14,40 @@ import jakarta.enterprise.event.Observes;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BannerPrinter {
 
+    private final String bannerPath;
+    private final boolean useCustomBanner;
     private static final Logger LOG = LoggerFactory.getLogger(BannerPrinter.class.getName());
-    
-    public void onStart(@Observes StartupEvent evt) {
-        String banner = loadBanner();
+
+    // Using custom config section because quarkus seems to update quarkus.banner.path after rendering in native mode.
+    public BannerPrinter(@ConfigProperty(name = "banner.path", defaultValue = "default_banner.txt")String bannerPath,
+                         @ConfigProperty(name = "banner.enabled", defaultValue = "false")boolean useCustomBanner) {
         
+        this.bannerPath = bannerPath;
+        this.useCustomBanner = useCustomBanner;
+    }
+
+    public void onStart(@Observes StartupEvent evt) {
+        if (!useCustomBanner) {
+            return;
+        }
+        
+        String banner = loadBanner();
+
         if (banner != null) {
             LOG.info("\n\n{}", banner.stripTrailing());
         } else {
-            LOG.warn("The banner.txt file was not found in classpath.");
+            LOG.warn("The [{}] file was not found in classpath.", bannerPath);
         }
     }
-    
+
     private String loadBanner() {
-        try(var is = BannerPrinter.class.getClassLoader().getResourceAsStream("banner.txt")) {
+        try (var is = BannerPrinter.class.getClassLoader().getResourceAsStream(bannerPath)) {
             if (is == null) {
                 return null;
             }
@@ -40,7 +55,7 @@ public class BannerPrinter {
                 return scanner.useDelimiter("\\A").next();
             }
         } catch (IOException ex) {
-            LOG.warn("Failed to load the banner.txt file.");
+            LOG.warn("Failed to load the [{}] file.", bannerPath);
             return null;
         }
     }
